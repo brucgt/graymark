8d3e6b70f717003c367786a2732d19d62e0f504b
jerud-o <ocampo.jerud.2001@gmail.com>
2022-08-08 01:59:47 +0000

    readme instruction

--- templates/pages/products.php
+++ templates/pages/products.php
@@ -30,13 +30,13 @@
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 relative p-4">
             <a href="#" class="block p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Quarry</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">[text]</p>
             </a>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/3 relative p-4">
             <a href="#" class="block p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"></h5>
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Garment</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">[text]</p>
             </a>
         </div>

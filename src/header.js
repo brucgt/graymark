cc573228b87acab7f3350c713ed57767de784c09
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-08-11 18:05:48 +0800

    header

--- src/header.js
+++ src/header.js
@@ -1,14 +1,30 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
 const navItem = document.querySelector(".nav-item");
-const dropToggle = document.querySelector(".dropdown-toggle");
-const dropDown = document.querySelector(".dropdown-menu");
+const dropToggles = document.querySelectorAll(".dropdown-toggle");
+// const dropDown = document.querySelector(".dropdown-menu");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("active");
  });
 
-dropToggle.addEventListener("click", () => {
+// dropToggle.addEventListener("click", () => {
+//   dropDown.classList.toggle("active");
+//   dropToggle.classList.toggle("active");
+//  });
+
+// for(let i = 0; i < dropToggles.length; i++){
+//   const dropDown = dropToggles[i].querySelector(".dropdown-menu");
+//   dropToggles[i].addEventListener("click", function (){
+//   dropDown.classList.toggle("active");
+//   dropToggles[i].classList.toggle("active");
+//   });
+// } 
+
+dropToggles.forEach(function(dropToggle){
+  const dropDown = dropToggle.querySelector(".dropdown-menu");
+  dropToggle.addEventListener("click", function (){
   dropDown.classList.toggle("active");
   dropToggle.classList.toggle("active");
- });
+  });
+});
--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -4,23 +4,38 @@
    </a>
    <ul class="nav-menu">
       <li class="nav-item"><a href="#">Home</a></li>
-      <li class="nav-item dropdown-toggle"><a href="#">Feature Prods</a>
+      <li class="nav-item dropdown-toggle"><a href="#">Feature Prods <i class= "fa-solid fa-sort-down"></i></a>
          <ul class="dropdown-menu">
             <li class="dropdown-item"><a href="#"></a>Agri Products</li>
             <li class="dropdown-item"><a href="#"></a>GM Market</li>
          </ul>
       </li>
-      <li class="nav-item dropdown-toggle"><a href="#">Mining & Quarry</a>
-         <ul>
+      <li class="nav-item dropdown-toggle"><a href="#">Mining & Quarry <i class= "fa-solid fa-sort-down"></i></a>
+         <ul class="dropdown-menu">
             <li class="dropdown-item"><a href="#">Minerals</a></li>
             <li class="dropdown-item"><a href="#">Quarry</a></li>
          </ul>
       </li>
-      <li class="nav-item"><a href="#">Garments</a></li>
-      <li class="nav-item"><a href="#">Facilities</a></li>
+      <li class="nav-item dropdown-toggle"><a href="#">Garments <i class= "fa-solid fa-sort-down"></i></a>
+         <ul class="dropdown-menu">
+            <li class="dropdown-item"><a href="#">Customized Tailoring</a></li>
+            <li class="dropdown-item"><a href="#">Alterations</a></li>
+            <li class="dropdown-item"><a href="#">Children's Wear Apparel</a></li>
+            <li class="dropdown-item"><a href="#">Sports Apparel</a></li>
+            <li class="dropdown-item"><a href="#">Fire & Traffic Personnel Uniform</a></li>
+            <li class="dropdown-item"><a href="#">Military Uniform</a></li>
+         </ul>
+      </li>
+      <li class="nav-item dropdown-toggle"><a href="#">Facilities <i class= "fa-solid fa-sort-down"></i></a>
+         <ul class="dropdown-menu">
+            <li class="dropdown-item"><a href="#">Corporate Office</a></li>
+            <li class="dropdown-item"><a href="#">Factory/Production</a></li>
+            <li class="dropdown-item"><a href="#">Main Market</a></li>
+         </ul>
+      </li>
       <li class="nav-item"><a href="#">About us</a></li>
    </ul>
-   <div class="nav-toggle my-auto">
+   <div class="nav-toggle my-auto lg:hidden ">
    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
    </svg>

c8ac7b2ebc7ddeb0a2aee374464403763ffdc3cf
Karlperlas <108713801+Karlperlas@users.noreply.github.com>
2022-08-12 16:21:17 +0800

    added nesting

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -3,15 +3,15 @@
       <img class="h-full py-1" src="<?= url('assets/img/company/logo/graymark-high-uality-logo-3x2-no-undertext.png')?>" alt="">
    </a>
    <ul class="nav-menu">
-      <li class="nav-item"><a href="#">Homepage</a></li>
-      <li class="nav-item"><a href="#">Products</a></li>
-      <li class="nav-item dropdown-toggle"><a href="#">About</a>
-         <ul class="dropdown-menu">
+      <li class="nav-item hover:underline underline-offset-2 "><a href="#">Homepage</a></li>
+      <li class="nav-item hover:underline underline-offset-2 "><a href="#">Products</a></li>
+      <li class="nav-item hover:underline underline-offset-2 dropdown-toggle"><a href="#">About</a>
+         <ul class="dropdown-menu z-10">
             <li class="dropdown-item"><a href="#">Our Company</a></li>
             <li class="dropdown-item"><a href="#">Our Valued Clients</a></li>
          </ul>
       </li>
-      <li class="nav-item"><a href="#">Contact us</a></li>
+      <li class="nav-item hover:underline underline-offset-2"><a href="#">Contact us</a></li>
    </ul>
    <div class="nav-toggle my-auto">
       <i class="fa-solid fa-bars text-2xl"></i>

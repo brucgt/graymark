cf780d7e241037259178e3d3c2aa7fe655a6d6b2
Jake Lunaria Flores <90809353+Tsokolatee@users.noreply.github.com>
2022-08-12 17:35:30 +0800

    progress

--- templates/pages/about.php
+++ templates/pages/about.php
@@ -10,7 +10,7 @@
     .mapouter {
         position: relative;
         text-align: right;
-        width: 600px;
+        width: 50%;
         height: 500px;
     }
 
@@ -41,7 +41,7 @@
             <h1>GRAYMARK INTERNATIONAL SOURCING CORPORATION</h1>
         </div>
 
-        <div class="flex flex-col m-auto pb-[180px] w-8/12 text-white">
+        <div class="flex flex-col m-auto pb-[70px] w-[85%] text-white md:pb-[100px] md:w-8/12">
             <p class="about-content">Graymark International Sourcing Corporation was registered with Securities and Exchange Commission on January 7, 2008 with authorized capital stock of One Million Pesos equivalent to 1,000 shares. The company’s primary purpose is to engage in the business of Mineral, Sand and Gravel, Mining and trading of goods such as handicrafts, apparel and wearing accessories on wholesale/retail and exporting basis.</p>
         </div>
     </div>
@@ -50,69 +50,70 @@
         <h1 class="about-secondary-title">OUR DIRECTORS</h1>
     </div>
 
-    <div class="flex flex-row justify-center content-center m-auto p-5">
+        <div class="flex flex-wrap justify-center content-center m-auto">
 
-        <div class="about-primary-card">
-            <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
-            <div class="p-5 text-center">
-                <h1 class="text-xl font-bold">MARILOU J. SUPERIO</h1>
-                <p class="mb-3 about-card-content ">Chairman</p>
+            <div class="about-primary-card">
+                <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
+                <div class="p-5 text-center">
+                    <h1 class="text-xl font-bold">MARILOU J. SUPERIO</h1>
+                    <p class="mb-3 about-card-content ">Chairman</p>
+                </div>
             </div>
-        </div>
 
-        <div class="about-primary-card">
-            <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
-            <div class="p-5 text-center mt-auto">
-                <h1 class="text-xl font-bold">GIOVANNE S. SUPERIO</h1>
-                <p class="mb-3 about-card-content ">President</p>
+            <div class="about-primary-card">
+                <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
+                <div class="p-5 text-center mt-auto">
+                    <h1 class="text-xl font-bold">GIOVANNE S. SUPERIO</h1>
+                    <p class="mb-3 about-card-content ">President</p>
+                </div>
             </div>
-        </div>
 
-        <div class="about-primary-card">
-            <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
-            <div class="p-5 text-center mt-auto">
-                <h1 class="text-xl font-bold">ROGELIO ABELEDA JR.</h1>
-                <p class="mb-3 about-card-content ">Vice-President</p>
+            <div class="about-primary-card">
+                <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
+                <div class="p-5 text-center mt-auto">
+                    <h1 class="text-xl font-bold">ROGELIO ABELEDA JR.</h1>
+                    <p class="mb-3 about-card-content ">Vice-President</p>
+                </div>
             </div>
-        </div>
 
-        <div class="about-primary-card">
-            <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
-            <div class="p-5 text-center mt-auto">
-                <h1 class="text-xl font-bold">GRAZIELLE MAE J. SUPERIO</h1>
-                <p class="mb-3 about-card-content ">CORPORATE SECRETARY</p>
+            <div class="about-primary-card">
+                <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
+                <div class="p-5 text-center mt-auto">
+                    <h1 class="text-xl font-bold">GRAZIELLE MAE J. SUPERIO</h1>
+                    <p class="mb-3 about-card-content ">CORPORATE SECRETARY</p>
+                </div>
             </div>
-        </div>
 
-        <div class="about-primary-card">
-            <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
-            <div class="p-5 text-center mt-auto">
-                <h1 class="text-xl font-bold">JAMES PAUL S. BERNARDO</h1>
-                <p class="mb-3 about-card-content ">Sales & Outsourcing Specialist</p>
+            <div class="about-primary-card">
+                <img class="rounded-t-lg" src="<?= url("assets\img\others\profile.png") ?>" alt="">
+                <div class="p-5 text-center mt-auto">
+                    <h1 class="text-xl font-bold">JAMES PAUL S. BERNARDO</h1>
+                    <p class="mb-3 about-card-content ">Sales & Outsourcing Specialist</p>
+                </div>
             </div>
-        </div>
 
     </div>
 
-    <div class="bg-slate-100 p-10 bottom-dev mt-5">
-        <div class="about-secondary-title">
+    <div class="bg-slate-100 p-1 bottom-dev mt-5 lg:p-10">
+        <div class="text-3xl font-bold text-center pt-12 text-secondary-dark md:text-5xl md:p-10">
             <h1>OUR LOCATION</h1>
         </div>
 
-        <div class="flex flex-col items-center justify-center m-auto my-5 bg-white rounded-lg border shadow-md w-full overflow-hidden md:flex-row md:w-3/4">
-            <div class="flex flex-col justify-between p-4 leading-normal mr-10 text-primary-green">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">BUSINESS ADDRESS</h5>
+        <div class="flex flex-col items-center justify-center m-auto my-5 bg-white rounded-lg border shadow-md w-[97%] overflow-hidden sm:w-[80%]  md:w-[75%] lg:w-[95%] xl:w-[80%]  lg:flex-row">
+            <div class="flex flex-col justify-between p-2 leading-normal text-primary-green lg:w-1/2">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">BUSINESS ADDRESS</h5>
                 <p class="mb-3 font-normal text-gray-700">1902A EAST TOWER 19TH FLOOR, PHIL STOCK EXCHANGE BLDG, PASIG CITY, PHILIPPINES.</p>
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">CONTACT DETAILS</h5>
                 <p class="mb-3 font-bold text-gray-700">LANDLINE:</p>
                 <p class="mb-3 font-normal text-gray-700">(+63) 286307484</p>
                 <p class="mb-3 font-bold text-gray-700">MOBILE:</p>
-                <p class="mb-3 font-normal text-gray-700">(+63) 97-778-6426</p>
+                <p class="mb-3 font-normal text-gray-700">(+63) 97 - 7786 - 426</p>
                 <p class="mb-3 font-bold text-gray-700">E-MAIL ADDRESSES:</p>
                 <p class="mb-3 font-normal text-gray-700">malou.superio@graymark.com.ph</p>
                 <p class="mb-3 font-normal text-gray-700">girlie.espiritu@graymark.com.ph</p>
             </div>
-            <div class="mapouter right-0">
+            
+            <div class="mapouter right-0 hidden lg:block">
                 <div class="gmap_canvas"><iframe class="gmap_iframe" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=600&amp;hl=en&amp;q=PHIL STOCK EXCHANGE BLDG, PASIG CITY, PHILIPPINES.&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe><a href="https://embedmapgenerator.com/">embed google maps in website</a></div>
             </div>
         </div>
--- templates/pages/clients.php
+++ templates/pages/clients.php
@@ -10,19 +10,41 @@
     .top-dev {
         clip-path: polygon(0 0, 100% 0, 100% calc(100% - 50px), 50% 100%, 0 calc(100% - 50px));
     }
-
     .bottom-dev {
         clip-path: polygon(0 50px, 50% 0, 100% 50px, 100% 100%, 0 100%);
         margin-top: -40px;
     }
+    .address{
+        margin-top: auto;
+    }
+    .address::before {
+        content: "\f3c5";
+        font-family: "font awesome 5 free";
+        font-weight: bold;
+        margin-right: 10px;
+    }
+    .contact::before {
+        content: "\f095";
+        font-family: "font awesome 5 free";
+        font-weight: bold;
+        margin-right: 10px;
+    }
+    .fax::before {
+        content: "\f1ac";
+        font-family: "font awesome 5 free";
+        font-weight: bold;
+        margin-right: 10px;
+    }
+
+    
 </style>
 
 <body>
 
     <div style="background-image:url(<?= url('assets/img/others/bg-client.png') ?>);" class="bg-cover bg-center bg-no-repeat top-dev">
         <div class="flex flex-col text-center">
-            <h1 class="text-7xl font-bold text-center p-20 text-white">OUR VALUED CLIENTS</h1>
-            <div class="text-3xl font-normal pb-20 text-white">
+            <h1 class="text-4xl font-bold text-center text-white p-10 md:p-20 md:text-7xl">OUR VALUED CLIENTS</h1>
+            <div class="text-xl font-normal pb-20 text-white md:text-3xl">
                 <ol>
                     <li class="p-3">FOREMOST FOODS INTERNATIONAL INC.</li>
                     <li class="p-3">INTERNATIONAL INTIMATES</li>
@@ -37,103 +59,103 @@
     </div>
 
     <div class="flex justify-center content-center m-auto">
-        <h1 class="text-5xl font-bold text-center p-10">ASSOCIATED PARTNERS AND MEMBERS</h1>
+        <h1 class="text-4xl font-bold text-center p-5 md:text-5xl md:p-10">ASSOCIATED PARTNERS AND MEMBERS</h1>
     </div>
 
-    <div class="flex flex-wrap justify-center content-center">
+    <div class="flex flex-wrap justify-center content-center lg:pb-5">
         <div class="about-secondary-card ">
             <h5 class="about-card-title">AAA Garments & Textile Mfg Inc.</h5>
-            <p class="about-card-content ">Supplier</p>
-            <p class="about-card-content ">ADDRESS: Warehouse No.3, Republic Tube Compound, Km 16, Pamplona, Las Piñas City.</p>
-            <p class="about-card-content ">TEL. : 632-8751487 / 632-873041</p>
+            <p class="about-card-content2 ">Supplier</p>
+            <p class="about-card-content2 address">Warehouse No.3, Republic Tube Compound, Km 16, Pamplona, Las Piñas City.</p>
+            <p class="about-card-content2 contact">632-8751487 / 632-873041</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">SYON, Inc.</h5>
-            <p class="about-card-content ">Supplier</p>
-            <p class="about-card-content ">ADDRESS: Lot 1 E. Maguyam Road, Maguyam, Silang Cavite.</p>
-            <p class="about-card-content ">TEL. : 632-529-8350</p>
+            <p class="about-card-content2 ">Supplier</p>
+            <p class="about-card-content2 address">Lot 1 E. Maguyam Road, Maguyam, Silang Cavite.</p>
+            <p class="about-card-content2 contact">632-529-8350</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">H & S Corporation</h5>
-            <p class="about-card-content ">ADDRESS: Building No. 2056 Corregidor Highway Ilanin Forest, East District Subic Bay Freeport Zone, Philippines</p>
+            <p class="about-card-content2 address">Building No. 2056 Corregidor Highway Ilanin Forest, East District Subic Bay Freeport Zone, Philippines</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">SHAN Craft Corporation</h5>
-            <p class="about-card-content ">Supplier</p>
-            <p class="about-card-content ">ADDRESS: Purok 4, Brgy Cubacub, Mandaue City, Cebu. </p>
+            <p class="about-card-content2">Supplier</p>
+            <p class="about-card-content2 address">Purok 4, Brgy Cubacub, Mandaue City, Cebu. </p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">CEBU Asia Trade Center Mfg. Inc.</h5>
-            <p class="about-card-content ">Supplier</p>
-            <p class="about-card-content ">PO 43 H. Abellana Street, Basak, Mandaue City, Cebu, Philippines.</p>
+            <p class="about-card-content2 ">Supplier</p>
+            <p class="about-card-content2 address">PO 43 H. Abellana Street, Basak, Mandaue City, Cebu, Philippines.</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">SGS Philippines, QC/Technician</h5>
-            <p class="about-card-content ">Partner</p>
-            <p class="about-card-content ">ADDRESS: 2nd Floor, Alegria Building, 229 Chino Roces Avenue, Makati City.</p>
-            <p class="about-card-content ">TEL. : 632-784-9400</p>
+            <p class="about-card-content2 ">Partner</p>
+            <p class="about-card-content2 address">2nd Floor, Alegria Building, 229 Chino Roces Avenue, Makati City.</p>
+            <p class="about-card-content2 contact">632-784-9400</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">Intertek Testing Services Philippines, Inc., QC/Technical</h5>
-            <p class="about-card-content ">Partner</p>
-            <p class="about-card-content ">ADDRESS: 2307 Don Chino Roces Ave., Ext. Brgy Magallanes, Makati City, Philippines, 1231.</p>
-            <p class="about-card-content ">TEL. : 632-819-5841/47</p>
-            <p class="about-card-content ">FAX. : 632-893-833</p>
+            <p class="about-card-content2 ">Partner</p>
+            <p class="about-card-content2 address">2307 Don Chino Roces Ave., Ext. Brgy Magallanes, Makati City, Philippines, 1231.</p>
+            <p class="about-card-content2 contact">632-819-5841/47</p>
+            <p class="about-card-content2 fax">632-893-833</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">American President Line</h5>
-            <p class="about-card-content ">Supplier</p>
-            <p class="about-card-content ">ADDRESS: 9th Floor , North Quadrant One E-Com Center Harbor Drive, Mall of Asia Complex, Pasay City.</p>
-            <p class="about-card-content ">TEL. : 632-479-5000</p>
+            <p class="about-card-content2 ">Supplier</p>
+            <p class="about-card-content2 address">9th Floor , North Quadrant One E-Com Center Harbor Drive, Mall of Asia Complex, Pasay City.</p>
+            <p class="about-card-content2 contact">632-479-5000</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">Associated Freight Consolidation, Inc.</h5>
-            <p class="about-card-content ">Logistic Partner</p>
-            <p class="about-card-content ">ADDRESS: Kaingin Road, Multinational Avenue, Sto. Nino Parañaque City.</p>
-            <p class="about-card-content ">TEL. : 632-822-1027 / 632-822-102</p>
+            <p class="about-card-content2 ">Logistic Partner</p>
+            <p class="about-card-content2 address">Kaingin Road, Multinational Avenue, Sto. Nino Parañaque City.</p>
+            <p class="about-card-content2 contact">632-822-1027 / 632-822-102</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">Eagle Express Lines, Inc.</h5>
-            <p class="about-card-content ">ADDRESS: Bldg. A, Cargo Village Complex, NAIA Avenue, Parañaque, 1701 Metro Manila</p>
-            <p class="about-card-content ">TEL. : (632) 7900 4900</p>
+            <p class="about-card-content2 address">Bldg. A, Cargo Village Complex, NAIA Avenue, Parañaque, 1701 Metro Manila</p>
+            <p class="about-card-content2 contact">(632) 7900 4900</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">PNB (Philippine National Bank)</h5>
-            <p class="about-card-content ">Banking Partner</p>
-            <p class="about-card-content ">ADDRESS: Morayta Branch, Nicanor Reyes Street, Sampaloc, Manila.</p>
-            <p class="about-card-content ">TEL. : 632-733-3511 / 632-735457</p>
+            <p class="about-card-content2 ">Banking Partner</p>
+            <p class="about-card-content2 address">Morayta Branch, Nicanor Reyes Street, Sampaloc, Manila.</p>
+            <p class="about-card-content2 contact">632-733-3511 / 632-735457</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">Union Bank of the Philippines</h5>
-            <p class="about-card-content ">Banking Partner</p>
-            <p class="about-card-content ">ADDRESS: Tektite Branch, PSE Building, Ortigas Center, Pasig City.</p>
-            <p class="about-card-content ">TEL. : 632-6373279 to 80</p>
+            <p class="about-card-content2 ">Banking Partner</p>
+            <p class="about-card-content2 address">Tektite Branch, PSE Building, Ortigas Center, Pasig City.</p>
+            <p class="about-card-content2 contact">632-6373279 to 80</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">BDO(Banco De Oro Unibank)</h5>
-            <p class="about-card-content ">Banking Partner</p>
-            <p class="about-card-content ">ADDRESS: Pasig-Meralco Brand, Ground Floor, One Corporate Centre, Julia A Vargas Corner Meralco Avenue, Ortigas Center, Pasig City.</p>
-            <p class="about-card-content ">TEL. : 632-4708021 / 632-4709780</p>
-            <p class="about-card-content ">FAX. : 632-6384864</p>
+            <p class="about-card-content2 ">Banking Partner</p>
+            <p class="about-card-content2 address">Pasig-Meralco Brand, Ground Floor, One Corporate Centre, Julia A Vargas Corner Meralco Avenue, Ortigas Center, Pasig City.</p>
+            <p class="about-card-content2 contact">632-4708021 / 632-4709780</p>
+            <p class="about-card-content2 fax">632-6384864</p>
         </div>
 
         <div class="about-secondary-card">
             <h5 class="about-card-title">UCPB (United Coconut Planters Bank)</h5>
-            <p class="about-card-content ">Banking Partner</p>
-            <p class="about-card-content ">ADDRESS: Tektite Branch, Ground Floor, PSE Building – West Tower, Exchange Road, Ortigas Center, Pasig City.</p>
-            <p class="about-card-content ">TEL. : 632-6386756 to 80</p>
+            <p class="about-card-content2 ">Banking Partner</p>
+            <p class="about-card-content2 address">Tektite Branch, Ground Floor, PSE Building – West Tower, Exchange Road, Ortigas Center, Pasig City.</p>
+            <p class="about-card-content2 contact">632-6386756 to 80</p>
         </div>
     </div>
 

7c9d3f082e7e2c22cec293fe573ac34a6ce4b1a1
Jake Lunaria Flores <90809353+Tsokolatee@users.noreply.github.com>
2022-09-06 14:30:42 +0800

    progress

--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -1,9 +1,9 @@
 <nav class="h-16 bg-gray-50 shadow-md flex justify-between px-4 sticky top-0 z-50">
-   <a href="#">
+   <a href="<?= url('') ?>">
       <img class="h-full py-1" src="<?= url('assets/img/company/logo/graymark-high-uality-logo-3x2-no-undertext.png')?>" alt="">
    </a>
    <ul class="nav-menu z-20">
-      <li class="nav-item hover:underline underline-offset-8"><a href="<?= url('') ?>">Homepage</a></li>
+      <li class="nav-item hover:underline underline-offset-8"><a href="<?= url('') ?>">Home</a></li>
       <li class="nav-item hover:underline underline-offset-8"><a href="<?= url('/products') ?>">Products</a></li>
       <li class="nav-item hover:underline underline-offset-8 dropdown-toggle"><a href="#" class="nav-link">About <i class="fa-solid fa-caret-down"></i></a>
          <ul class="dropdown-menu z-10">
